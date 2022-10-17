02a5ced3da5cd75086b7fc4443ad19de517df630
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2022-10-16 23:44:43 -0300

    Add documentação em livroController

--- src/main/java/org/serratec/backend/library/controller/LivroController.java
+++ src/main/java/org/serratec/backend/library/controller/LivroController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
@@ -22,22 +26,45 @@ public class LivroController {
 	LivroRepository livroRepository;
 	
 	@GetMapping
+	@ApiOperation(value="Lista todos os livros", notes="Listagem de livros")
+	@ApiResponses(value= {
+	@ApiResponse(code=200, message="Retorna todos os livros"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=404, message="Recurso não encontrado"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),
+	})
 	public List<Livro> getListaLivros(){
 		return livroRepository.findAll();
 	}
 	
 	@GetMapping("/isbn/{isbn}")
+	@ApiOperation(value="Retorna livro de acordo com ISBN", notes="Busca de livro")
+	@ApiResponses(value= {
+	@ApiResponse(code=200, message="Retorna livro com o ISBN especificado"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=404, message="Recurso não encontrado"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),
+	})
 	public Livro pesquisarPorISBN(@PathVariable String isbn) {
 		return livroRepository.findByIsbn(isbn); 
 	}
 	
-//	@GetMapping("/cat/{titulo}")
-//	public List<Livro> ordernarCategoria(@PathVariable String titulo) {
-//		return livroRepository.findByTituloOrderByCategoria(titulo);
-//	}
+	@GetMapping("/cat/{titulo}")
+	public List<Livro> ordernarCategoria(@PathVariable String titulo) {
+		return livroRepository.findByTituloOrderByCategoria(titulo);
+	}
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
+	@ApiOperation(value="Insere um novo livro", notes="Inserção de livro")
+	@ApiResponses(value= {
+	@ApiResponse(code=201, message="Novo livro inserido com sucesso"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),
+	})
 	public Livro adicionarLivro(@RequestBody Livro novo_livro) {
 		return livroRepository.save(novo_livro);
 	}
